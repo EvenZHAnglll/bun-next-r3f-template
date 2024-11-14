85e374dd026b79b948cc67291de720c8d0ad53ad
even <ezyoung@live.com>
2024-11-14 17:52:40 +0800

    remove unused files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,6 @@
 
 import { Canvas3D } from "@/component/Canvas3D";
 
-// import dynamic from 'next/dynamic'
-// const Canvas3D = dynamic(() => import('@/component/Canvas3D').then((mod) => mod.Canvas3D), { ssr: false })
-
 export default function Home() {
   return (
     <div className="bg-gray-800 w-full h-full" >
